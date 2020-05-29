6cb116e8f80f091139ccb0786af0d582d34ad497
Jonathan Marsh <hello@jonmarsh.dev>
2020-05-29 21:59:29 +0100

    MP-5: Refactor Config class
    Fix anti-pattern by removing initialisation function of what is essentially a utility class

--- src/main/java/dev/jonmarsh/minecartportal/Config.java
+++ src/main/java/dev/jonmarsh/minecartportal/Config.java
@@ -7,30 +7,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 import static org.bukkit.Bukkit.getServer;
 
 public final class Config {
-    private static Config config = null;
+    static public String OverworldName = "world";
+    static public String NetherName = "world_nether";
+    static public int Scale = 8;
+    static public int PortalY = 64;
 
     static public World Overworld;
     static public World Nether;
-    static public int Scale;
-    static public int PortalY;
 
-    private Config(JavaPlugin plugin) {
+    static public void initialise(JavaPlugin plugin) {
         final FileConfiguration configuration = plugin.getConfig();
         configuration.options().copyDefaults();
         plugin.saveDefaultConfig();
-        Overworld = getServer().getWorld(configuration.getString("Overworld", "world"));
-        Nether = getServer().getWorld(configuration.getString("Nether", "world_nether"));
-        Scale = plugin.getConfig().getInt("Scale", 8);
-        PortalY = plugin.getConfig().getInt("PortalY", 64);
-    }
-
-    static public void initialise(JavaPlugin plugin) {
-        if (config == null) {
-            config = new Config(plugin);
-        }
-    }
-
-    public static Config getInstance() {
-        return config;
+        OverworldName = configuration.getString("Overworld", OverworldName);
+        NetherName = configuration.getString("Nether", NetherName);
+        Overworld = getServer().getWorld(OverworldName);
+        Nether = getServer().getWorld(NetherName);
+        Scale = configuration.getInt("Scale", Scale);
+        PortalY = configuration.getInt("PortalY", PortalY);
     }
 }
