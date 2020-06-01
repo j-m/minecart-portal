64e66cd40a72cf18085eef814562c75d62d15286
Jonathan Marsh <hello@jonmarsh.dev>
2020-06-02 00:58:00 +0100

    MP-6: Remove superfluous info class. Teleport minecart based on velocity

--- src/main/java/dev/jonmarsh/minecartportal/DismountRemount.java
+++ src/main/java/dev/jonmarsh/minecartportal/DismountRemount.java
@@ -1,12 +1,14 @@
 package dev.jonmarsh.minecartportal;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 
 public final class DismountRemount {
-    public static HashMap<Entity, VehicleInfo> passengerVehiclePair = new HashMap<>();
+    public static HashMap<Entity, Entity> passengerVehiclePair = new HashMap<>();
+    public static HashMap<Entity, Vector> vehicleVelocityPair = new HashMap<>();
 
     public static void dismount(Entity vehicle) {
         if (vehicle.getPortalCooldown() != 0) {
@@ -14,30 +16,62 @@ public final class DismountRemount {
             return;
         }
 
-        VehicleInfo vehicleInfo = new VehicleInfo(vehicle);
-        Output.ServerLog("Passenger list: " + vehicleInfo.passengers);
+        Output.ServerLog("Prior dismounts, vehicle has velocity: " + vehicle.getVelocity().toString());
+        vehicleVelocityPair.put(vehicle, vehicle.getVelocity());
 
-        for (Entity passenger: vehicleInfo.passengers) {
+        Output.ServerLog("Passenger list: " + vehicle.getPassengers());
+
+        for (Entity passenger: vehicle.getPassengers()) {
             Output.ServerLog("Removing passenger: " + passenger.getName());
+            passengerVehiclePair.put(passenger, vehicle);
+            passenger.setPortalCooldown(0);
             passenger.leaveVehicle();
-            passengerVehiclePair.put(passenger, vehicleInfo);
         }
-        Output.ServerLog("Velocity: " + vehicleInfo.vehicle.getVelocity());
-        vehicleInfo.vehicle.setVelocity(vehicleInfo.velocity);
-        Output.ServerLog("Velocity: " + vehicleInfo.vehicle.getVelocity());
     }
 
     public static void remount(Entity passenger) {
-        final VehicleInfo vehicleInfo = passengerVehiclePair.get(passenger);
-        if (vehicleInfo == null) {
+        Output.ServerLog("Attempt to remount passenger: " + passenger.getName());
+        final Entity vehicle = passengerVehiclePair.get(passenger);
+        if (vehicle == null) {
             Output.ServerLog("Did not find a vehicle for: " + passenger);
             return;
         }
-        vehicleInfo.vehicle.setPortalCooldown(20);
-        Output.ServerLog("Remounting passenger: " + passenger.getName());
-        passengerVehiclePair.remove(passenger);
-        //passenger.teleport(vehicleInfo.vehicle.getLocation());
-        vehicleInfo.vehicle.addPassenger(passenger);
 
+        moveVehicleOutOfPortal(vehicle);
+
+        Output.ServerLog("Remounting passenger: " + passenger.getName());
+        vehicle.addPassenger(passenger);
+        passengerVehiclePair.remove(passenger);
+        passenger.setPortalCooldown(0);
+    }
+
+    private static void moveVehicleOutOfPortal(final Entity vehicle) {
+        final Vector vehicleVelocity = vehicleVelocityPair.get(vehicle);
+        if(vehicleVelocity == null) {
+            Output.ServerLog("Did not find a vehicle velocity for: " + vehicle);
+            return;
+        }
+
+        final Vector negatedVehicleVelocity = negateVelocity(vehicleVelocity);
+
+        Output.ServerLog("Moving vehicle from: " + vehicle.getLocation().toString());
+        vehicle.teleport(locationAddVelocity(vehicle.getLocation(), negatedVehicleVelocity));
+        Output.ServerLog("Moving vehicle to: " + vehicle.getLocation().toString());
+
+        Output.ServerLog("Setting vehicle velocity to: " + negatedVehicleVelocity);
+        vehicle.setVelocity(negatedVehicleVelocity);
+        vehicleVelocityPair.remove(vehicle);
+        vehicle.setPortalCooldown(0);
+    }
+
+    private static Vector negateVelocity(final Vector velocity) {
+        return new Vector(velocity.getX() * -1, velocity.getY(), velocity.getZ() * -1);
+    }
+
+    private static Location locationAddVelocity(final Location location, final Vector velocity) {
+        final double x = location.getX() + velocity.getX();
+        final double y = location.getY() + velocity.getY();
+        final double z = location.getZ() + velocity.getZ();
+        return new Location(location.getWorld(), x, y, z);
     }
 }
--- src/main/java/dev/jonmarsh/minecartportal/event/VehicleOutPortal.java
+++ src/main/java/dev/jonmarsh/minecartportal/event/VehicleOutPortal.java
@@ -3,6 +3,7 @@ package dev.jonmarsh.minecartportal.event;
 import dev.jonmarsh.minecartportal.DismountRemount;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPortalEvent;
 import org.bukkit.event.entity.EntityPortalExitEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 
@@ -15,6 +16,11 @@ public class VehicleOutPortal implements Listener {
 
     @EventHandler
     public void onEntityOutOfPortal(final EntityPortalExitEvent event) {
+        DismountRemount.remount(event.getEntity());
+    }
+
+    @EventHandler
+    public void onEntityOutOfPortal(final EntityPortalEvent event) {
         DismountRemount.remount(event.getEntity());
     }
 }
